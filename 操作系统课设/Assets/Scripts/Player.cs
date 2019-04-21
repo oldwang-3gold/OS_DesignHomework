41d57cfdbff59b774063121756b3cc161a9aef0d
oldwang-3gold <32608811+oldwang-3gold@users.noreply.github.com>
2019-04-21 21:30:36 +0800

    add player control

---
+++ 操作系统课设/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator anim;
+    public float moveSpeed=3f;
+    public float rotateSpeed = 3f;
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {   
+        float v = Input.GetAxis("Vertical");
+     //   float speed = v * Time.fixedDeltaTime * moveSpeed;
+     //   Debug.Log(speed);
+        anim.SetFloat("Speed",v);
+        transform.Translate(Vector3.forward*v*Time.fixedDeltaTime*moveSpeed);
+        float h = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up*h*Time.fixedDeltaTime*rotateSpeed,Space.Self);
+    }
+}

a364c1e31d6ca86a9ee565141c4b91c7c4b88d66
oldwang-3gold <32608811+oldwang-3gold@users.noreply.github.com>
2019-04-21 22:57:56 +0800

    add Server and client script in VS2019

---
+++ 服务器端Server(VS2019)/服务器端Server(VS2019)/Server/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 服务器端Server_VS2019_.Server
+{
+    class Client
+    {
+        private Socket clientSocket;
+        private Server server;
+        public Client() { }
+
+        public Client(Socket clientSocket,Server server)
+        {
+            this.clientSocket = clientSocket;
+            this.server = server;
+        }
+
+        public void Start()//开启监听服务器端的数据
+        {
+            clientSocket.BeginReceive(null, 0, 0, SocketFlags.None, ReceiveCallBack,null);
+        }
+
+        private void ReceiveCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                int count = clientSocket.EndReceive(ar);
+                if (count == 0) //接受到数据长度为0 说明断开连接
+                {
+                    Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Close();
+            }
+            
+        }
+
+        private void Close()
+        {
+            if (clientSocket != null)
+            {
+                clientSocket.Close();
+            }
+            server.RemoveClient(this);
+        }
+        
+    }
+}
---
+++ 服务器端Server(VS2019)/服务器端Server(VS2019)/Server/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 服务器端Server_VS2019_.Server
+{
+    class Server
+    {
+        private IPEndPoint ipEndPoint;
+        private Socket serverSocket;
+        private List<Client> clientLists;
+        public Server(){ }
+
+        public Server(string ipStr, int port)
+        {
+            SetIpAndPort(ipStr,port);
+        }
+
+        public void SetIpAndPort(string ipStr, int port)
+        {
+            ipEndPoint = new IPEndPoint(IPAddress.Parse(ipStr), port);//创建 连接ip地址和端口号
+        }
+
+        public void Start()//启动监听
+        {
+            serverSocket=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            serverSocket.Bind(ipEndPoint);//绑定ip地址和端口号
+            serverSocket.Listen(0);//监听
+            serverSocket.BeginAccept(AcceptCallBack, null); //接受客户端的连接
+        }
+        /// <summary>
+        /// 回调函数
+        /// </summary>
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = serverSocket.EndAccept(ar);//接受到与服务器端连接的客户端
+            Client client=new Client(clientSocket,this);
+            client.Start();
+            clientLists.Add(client);
+        }
+
+        public void RemoveClient(Client client)
+        {
+            lock (clientLists)//防止线程之间处理异常
+            {
+                clientLists.Remove(client);
+            }
+            
+        }
+    }
+}

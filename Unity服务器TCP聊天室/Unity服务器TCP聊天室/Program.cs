05f8c8d06d6319c39f862bcf38afad0841b69550
oldwang-3gold <32608811+oldwang-3gold@users.noreply.github.com>
2019-04-27 10:36:26 +0800

    add find localIP method

--- Unity服务器TCP聊天室/Unity服务器TCP聊天室/Program.cs
+++ Unity服务器TCP聊天室/Unity服务器TCP聊天室/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,11 +33,38 @@ namespace Unity服务器TCP聊天室
                 clientList.Remove(temp);
             }
         }
+
+        public static string GetLocalIP()
+        {
+            try
+            {
+                string hostName = Dns.GetHostName(); //获取主机名
+                IPHostEntry ipHostEntry = Dns.GetHostEntry(hostName);
+                for (int i = 0; i < ipHostEntry.AddressList.Length; i++) //从IP地址列表筛选出ipv4类型的IP地址
+                {
+                    if (ipHostEntry.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        Console.WriteLine("本机ip地址为:"+ipHostEntry.AddressList[i].ToString());
+                        return ipHostEntry.AddressList[i].ToString();
+                    }
+                }
+
+                return "";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("获取本机IP出错:"+e.ToString());
+                return "";
+            }
+            
+        }
         static void Main(string[] args)
         {
             Socket tcpServer=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+
+            string ipAddress = GetLocalIP();
             //绑定IP地址使用本地的 通过控制台cmd中 ipconfig找到ipv4地址 端口号设置成四位数最好
-            tcpServer.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.104"), 7788));
+            tcpServer.Bind(new IPEndPoint(IPAddress.Parse(ipAddress), 7788));
             tcpServer.Listen(100);
             Console.WriteLine("server running");
 
@@ -47,8 +75,7 @@ namespace Unity服务器TCP聊天室
                 Console.WriteLine("a client connected!");
                 Client client=new Client(clientSocket);//把与每个客户端通信的逻辑在client类里实现               
                 clientList.Add(client);//添加到集合中
-            }
-            
+            }      
         }
     }
 }
